28c0d4d594c985e81e170e936180a1d7b7aae6de
sofyaandrade <sofya.bruzadelli17@gmail.com>
2025-03-27 21:58:05 -0300

    Projeto

--- olamundo/app/Models/Combustivel.php
+++ olamundo/app/Models/Combustivel.php
@@ -1,10 +1,27 @@
 <?php
 
-namespace App\Models;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-use Illuminate\Database\Eloquent\Model;
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('combustiveis', function (Blueprint $table) {
+            $table->id();
+            $table->string('combustivel');
+            $table->decimal('valor', 8, 2);
+            $table->decimal('distancia', 8, 2);
+            $table->decimal('consumo', 8, 2);
+            $table->decimal('litros_usados', 8, 2);
+            $table->decimal('gasto', 8, 2);
+            $table->timestamps();
+        });
+    }
 
-class Combustivel extends Model
-{
-    //
-}
+    public function down()
+    {
+        Schema::dropIfExists('combustiveis');
+    }
+};
+
